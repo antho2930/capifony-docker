0a5b2cbdafabd5e9b8e3189701446c9641aed50b
Jérémy CROMBEZ <jeremy.crombez@gmail.com>
2016-03-24 10:01:49 +0100

    use known_hosts in script

--- cap.sh
+++ cap.sh
@@ -2,10 +2,7 @@
 
 WORKDIR=$(pwd)
 SSH_AGENT=$(readlink -f $SSH_AUTH_SOCK)
-
-if [ $(command -v selinuxenabled >/dev/null 2>&1) -a $(selinuxenabled) ]; then
-    chcon -Rt svirt_sandbox_file_t $WORKDIR $SSH_AGENT
-fi
+KNOWN_HOSTS=$HOME/.ssh/known_hosts
 
 docker run \
     --rm \
@@ -13,5 +10,6 @@ docker run \
     -t \
     -v $WORKDIR:/root/workdir \
     -v $SSH_AGENT:/root/ssh-agent \
-    mjanser/capifony \
+    -v $KNOWN_HOSTS:/root/.ssh/known_hosts \
+    jcrombez/capifony \
     $@
